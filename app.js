b1952179e3fec9ff28cf593a02b5d5d77fd31d80
Stezi1 <zedek.daniel14@seznam.cz>
2026-05-30 16:36:47 +0200

    app.js

---
+++ app.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const connectDB = require('./config/db');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+connectDB();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 }
+}));
+
+
+app.use((req, res, next) => {
+    res.locals.user = req.session.user || null;
+    next();
+});
+
+
+app.get('/', (req, res) => {
+    res.send('<h1>EventZone backend je připraven! 🚀</h1>');
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server běží na http://localhost:${PORT}`);
+});
