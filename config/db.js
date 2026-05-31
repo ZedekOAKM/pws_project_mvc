4c526f8518f2b8e48741df204e4e4a967741cd6c
Stezi1 <zedek.daniel14@seznam.cz>
2026-05-31 16:51:25 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB připojeno: ${conn.connection.host}`);
+        console.log(`✅ MongoDB připojena: ${conn.connection.host}`);
     } catch (error) {
-        console.error(`Chyba připojení k databázi: ${error.message}`);
+        console.error(`❌ Chyba připojení k databázi: ${error.message}`);
         process.exit(1);
     }
 };
